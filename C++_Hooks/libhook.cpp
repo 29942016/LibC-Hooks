24421fbb5f96cab1280805d0d99999f6f3311ab7
Oliver <oliver.buckler@gmail.com>
2017-09-09 22:22:09 +0800

    clean

--- C++_Hooks/libhook.cpp
+++ C++_Hooks/libhook.cpp
@@ -8,6 +8,9 @@
 typedef int (*puts_type)(const char*);
 static puts_type real_puts = NULL;
 
+typedef ssize_t (*write_type)(int, const void*, size_t);
+static write_type real_write = NULL;
+
 typedef int (*uname_type)(struct utsname*);
 static uname_type real_uname = NULL;
 
@@ -28,29 +31,16 @@ int uname(struct utsname* info)
 	std::strcpy(info->domainname, "DOMAINNAME");
 }
 
-/*
-typedef ssize_t (*write_type)(int, const void*, size_t);
-static write_type real_write = NULL;
-
-
 ssize_t write(int fd, const void *buf, size_t count)
 {	
-	printf("writechars#:%lu\n", count);
-
 	real_write = (write_type)dlsym(RTLD_NEXT, "write");
 	real_write(fd, buf, count);	
 }
-*/
 
 int puts(const char* str)
 {
-    //printf("puts:chars#:%lu\n", strlen(str));
-    /* resolve the real puts function from glibc
-     * and pass the arguments.
-    */
-//	printf("[PUTS HOOK TRIGGERED]");
-//	printf("\n");
-//	fflush(stdout);
+	printf("[PUTS HOOK TRIGGERED]");
+	printf("\n");
 
     real_puts = (puts_type)dlsym(RTLD_NEXT, "puts");
     real_puts(str);
